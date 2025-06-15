8e54766cd379599a41181439ade18775da905073
Lesetja <kmalapane155@gmail.com>
2025-06-15 12:06:01 +0200

    code refactor

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+const operators = ["*", "/", "+", "-", "="]
+
 function add(a, b) {
     return a + b
 }
@@ -33,8 +36,6 @@ function operate(a, b, op) {
     }
 }
 
-const numbers = [0,1,2,3,4,5,6,7,8,9]
-const operators = ["*", "/", "+", "-"]
 
 numbers.forEach(number => {
     const button = document.createElement("button");
